ba4fd061a8b28aeb515cd54de1506947a97c1468
Josh Schell <jschell12@gmail.com>
2026-07-20 18:45:53 -0400

    style: gofmt keychain_other.go

--- internal/store/keychain_other.go
+++ internal/store/keychain_other.go
@@ -14,7 +14,7 @@ type unsupportedStore struct{}
 
 var errUnsupported = errors.New("scredmanager: keychain backend is only available on macOS")
 
-func (*unsupportedStore) Set(string, []byte) error      { return errUnsupported }
-func (*unsupportedStore) Get(string) ([]byte, error)    { return nil, errUnsupported }
-func (*unsupportedStore) Delete(string) error           { return errUnsupported }
-func (*unsupportedStore) Exists(string) (bool, error)   { return false, errUnsupported }
+func (*unsupportedStore) Set(string, []byte) error    { return errUnsupported }
+func (*unsupportedStore) Get(string) ([]byte, error)  { return nil, errUnsupported }
+func (*unsupportedStore) Delete(string) error         { return errUnsupported }
+func (*unsupportedStore) Exists(string) (bool, error) { return false, errUnsupported }
